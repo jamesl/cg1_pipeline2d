8e4e367c80f2510c3f46c2347a96997190fccc3e
James Loomis <james@ppcmac.local>
2009-01-24 16:09:32 -0500

    removed myFunction debug info

--- myFunctions.c
+++ myFunctions.c
@@ -61,7 +61,6 @@ struct window{
 void myBegin(GLenum mode) {
 	vertexmode = mode;
 	vertices.clear();
-	cout << vertices.size() << endl;
 }
 
 
@@ -90,9 +89,7 @@ void myVertex2f(float x, float y)
 	pt(1,0) = y;
 	pt(2,0) = 0.0;
 	pt(3,0) = 1.0;
-	//if(vertices.size()>=4) return;
 	vertices.push_back(pt);
-	cout << vertices.size() << endl;
 }
 
 
